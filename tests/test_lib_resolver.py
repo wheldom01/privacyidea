5787ef4eba17dff4fe56a9875c2b21e258934e74
Martin Wheldon <martin.wheldon@greenhills-it.co.uk>
2016-04-12 15:25:53 +0200

    Updated to support objectGUID
    Added test cases to test add/delete modification
    Updated to ensure add_user understands objectGUID

--- tests/test_lib_resolver.py
+++ tests/test_lib_resolver.py
@@ -12,6 +12,7 @@ PWFILE = "tests/testdata/passwords"
 from .base import MyTestCase
 import ldap3mock
 import responses
+import uuid
 from privacyidea.lib.resolvers.LDAPIdResolver import IdResolver as LDAPResolver
 from privacyidea.lib.resolvers.SQLIdResolver import IdResolver as SQLResolver
 from privacyidea.lib.resolvers.SCIMIdResolver import IdResolver as SCIMResolver
@@ -1006,6 +1007,54 @@ class LDAPResolverTestCase(MyTestCase):
         userinfo = y.getUserInfo(user_id)
         self.assertEqual(userinfo.get("givenname"), "Charlie")
         self.assertFalse(userinfo.get("email"))
+
+        # Now we delete the user with add_user
+        y.delete_user(user_id)
+        # Now there should be no achmed anymore
+        user_id = y.getUserId("achmed")
+        self.assertFalse(user_id)
+
+    @ldap3mock.activate
+    def test_14_add_user_update_delete_objectGUID(self):
+        ldap3mock.setLDAPDirectory(LDAPDirectory)
+        y = LDAPResolver()
+        y.loadConfig({'LDAPURI': 'ldap://localhost',
+                      'LDAPBASE': 'o=test',
+                      'BINDDN': 'cn=manager,ou=example,o=test',
+                      'BINDPW': 'ldaptest',
+                      'LOGINNAMEATTRIBUTE': 'cn',
+                      'LDAPSEARCHFILTER': '(cn=*)',
+                      'LDAPFILTER': '(&(cn=%s))',
+                      'USERINFO': '{ "username": "cn",'
+                                  '"phone" : "telephoneNumber", '
+                                  '"mobile" : "mobile",'
+                                  '"email" : "email",'
+                                  '"userid" : "objectGUID",'
+                                  '"password" : "userPassword",'
+                                  '"surname" : "sn", '
+                                  '"givenname" : "givenName", '
+                                  '"accountExpires": "accountExpires" }',
+                      'UIDTYPE': 'objectGUID',
+                      'NOREFERRALS': True
+        })
+
+        user = "achmed"
+        dn="cn={0},ou=example,o=test".format(user)
+        uid = uuid.uuid4().bytes
+        user_id = str(uuid.UUID(bytes_le=uid))
+
+        classes = ['top', 'inetOrgPerson']
+        attributes = {"username" : user,
+                      "surname" : "Ali",
+                      "userid" : uid, 
+                      "email" : "achmed.ali@example.com",
+                      "password" : "testing123",
+                      'mobile': ["1234", "45678"],
+                      "givenname" : "Achmed"}
+
+        # First we add the user with add_user 
+        r = y.add_user(user_id, classes, attributes, dn)
+        self.assertTrue(r)
 
         # Now we delete the user with add_user
         y.delete_user(user_id)
